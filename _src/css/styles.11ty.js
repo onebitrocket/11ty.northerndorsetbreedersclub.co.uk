71d8b001b62f52f0c9a47f6a444c8f7df10d32fe
Matt Tunney <mtunney@me.com>
2022-11-11 18:48:22 +0000

    fixes

--- _src/css/styles.11ty.js
+++ _src/css/styles.11ty.js
@@ -1,29 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const postcss = require('postcss');
+const fs = require("fs");
+const path = require("path");
+const postcss = require("postcss");
 
 // the file name as an entry point for postcss compilation
 // also used to define the output filename in our output /css folder.
 const fileName = "styles.css";
 
 module.exports = class {
-  async data () {
-    const rawFilepath = path.join(__dirname, `../_includes/postcss/${fileName}`);
+  async data() {
+    const rawFilepath = path.join(
+      __dirname,
+      `../_includes/postcss/${fileName}`
+    );
     return {
       permalink: `css/${fileName}`,
       eleventyExcludeFromCollections: true,
       rawFilepath,
-      rawCss: await fs.readFileSync(rawFilepath)
+      rawCss: await fs.readFileSync(rawFilepath),
     };
-  };
+  }
 
-  async render ({ rawCss, rawFilepath }) {
+  async render({ rawCss, rawFilepath }) {
     return await postcss([
-      require('postcss-normalize'),
-      require('postcss-nested'),
-      require('cssnano')
+      require("postcss-normalize"),
+      require("postcss-nested"),
+      require("cssnano"),
     ])
-    .process(rawCss, { from: rawFilepath })
-    .then(result => result.css);
-  };
-}
+      .process(rawCss, { from: rawFilepath })
+      .then((result) => result.css);
+  }
+};
